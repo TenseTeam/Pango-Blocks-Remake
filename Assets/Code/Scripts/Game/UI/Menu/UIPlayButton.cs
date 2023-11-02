c0929a5d8520bb19938eeca862b657a0d981b91b
zVoyder <zvoyder@protonmail.com>
2023-11-02 23:47:09 +0100

    working on UIPlayButton

---
+++ Assets/Code/Scripts/Game/UI/Menu/UIPlayButton.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectPBR.UI.Menu
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class UIPlayButton : MonoBehaviour
+    {
+        // TO DO: Button to check if the player has selected a Profile before allowing them to play
+    }
+}
